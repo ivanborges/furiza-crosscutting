ee1a34e36a11079a9f2090c057849aef6d912a8b
Ivan Borges <ivanborges@live.com>
2018-10-22 23:09:56 -0300

    Simplificação na configuração do log; adição de método AddObject no configurationbuilder.

---
+++ src/Furiza.Extensions.Configuration/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Furiza.Extensions.Configuration;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        /// <summary>
+        /// Adds object configuration provider to configuration builder.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="builder"></param>
+        /// <param name="configurationObject">Any object that carry configuration information.</param>
+        /// <returns></returns>
+        public static IConfigurationBuilder AddObject<T>(this IConfigurationBuilder builder, T configurationObject)
+            where T : class
+        {
+            return builder.Add(new JsonObjectConfigurationSource(configurationObject));
+        }
+    }
+}
--- src/Furiza.Logging/Microsoft.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
+++ src/Furiza.Logging/Microsoft.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -2,8 +2,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Exceptions;
+using Serilog.Formatting.Json;
 using System;
-using System.Linq;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
@@ -18,48 +19,41 @@ namespace Microsoft.Extensions.DependencyInjection
             switch (logConfiguration.Tool.Value)
             {
                 case LogTool.Serilog:
-                    ValidateSerilogStructure(logConfiguration);
+                    var loggerConfiguration = new LoggerConfiguration()
+                        .ReadFrom.Configuration(configuration.GetSection(nameof(LogConfiguration)))
+                        .Enrich.WithExceptionDetails();
+
+                    if (logConfiguration.WriteToType != LogWriteTo.Raw)
+                    {
+                        loggerConfiguration = loggerConfiguration
+                            .Enrich.FromLogContext()
+                            .Enrich.WithProcessId()
+                            .Enrich.WithProcessName()
+                            .Enrich.WithThreadId()
+                            .Enrich.WithEnvironmentUserName()
+                            .Enrich.WithMachineName();
+
+                        if (logConfiguration.WriteToType == LogWriteTo.File)
+                            loggerConfiguration = loggerConfiguration.WriteTo.Async(a => a.File($"logs/log-.txt", rollingInterval: RollingInterval.Day));
+                        else
+                            loggerConfiguration = loggerConfiguration.WriteTo.Async(a => a.Console(new JsonFormatter()));
+                    }
 
                     services.AddLogging(c =>
                     {
                         c.ClearProviders();
 
-                        Log.Logger = new LoggerConfiguration()
-                            .ReadFrom.Configuration(configuration.GetSection(nameof(LogConfiguration)))
-                            .CreateLogger();
+                        Log.Logger = loggerConfiguration.CreateLogger();
 
                         c.AddSerilog();
                     });
                     break;
+                case LogTool.Default:
                 default:
                     throw new InvalidOperationException($"Logging provider '{logConfiguration.Tool.Value}' unavailable.");
             }           
 
             return services;
-        }
-
-        private static void ValidateSerilogStructure(LogConfiguration logConfiguration)
-        {
-            if (logConfiguration.Serilog == null)
-                throw new InvalidOperationException($"Configuration section '{nameof(LogConfiguration)}.{nameof(LogConfiguration.Serilog)}' is missing.");
-
-            if (logConfiguration.Serilog.Using == null)
-                throw new InvalidOperationException($"Attribute '{nameof(LogConfiguration.Serilog.Using)}' for configuration section '{nameof(LogConfiguration)}.{nameof(LogConfiguration.Serilog)}' is missing.");
-
-            if (!logConfiguration.Serilog.Using.Any())
-                throw new InvalidOperationException($"Attribute '{nameof(LogConfiguration.Serilog.Using)}' for configuration section '{nameof(LogConfiguration)}.{nameof(LogConfiguration.Serilog)}' must have at least 1 serilog package reference.");
-
-            if (logConfiguration.Serilog.MinimumLevel == null)
-                throw new InvalidOperationException($"Configuration section '{nameof(LogConfiguration)}.{nameof(LogConfiguration.Serilog)}.{nameof(LogConfiguration.Serilog.MinimumLevel)}' is missing.");
-
-            if (string.IsNullOrWhiteSpace(logConfiguration.Serilog.MinimumLevel.Default))
-                throw new InvalidOperationException($"Attribute '{nameof(LogConfiguration.Serilog.MinimumLevel.Default)}' for configuration section '{nameof(LogConfiguration)}.{nameof(LogConfiguration.Serilog)}.{nameof(LogConfiguration.Serilog.MinimumLevel)}' is missing.");
-
-            if (logConfiguration.Serilog.WriteTo == null)
-                throw new InvalidOperationException($"Configuration section '{nameof(LogConfiguration)}.{nameof(LogConfiguration.Serilog)}.{nameof(LogConfiguration.Serilog.WriteTo)}' is missing.");
-
-            if (!logConfiguration.Serilog.WriteTo.Any())
-                throw new InvalidOperationException($"Configuration section '{nameof(LogConfiguration)}.{nameof(LogConfiguration.Serilog)}.{nameof(LogConfiguration.Serilog.WriteTo)}' must have at least 1 logging output.");
         }
     }
 }
