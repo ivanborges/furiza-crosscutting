22873cc49f43034b28109fde92029afc325b8fda
Ivan Borges <ivanborges@live.com>
2018-11-14 19:15:21 -0200

    Inclusão de listas de propriedades a serem ignoradas durante a serialização do objeto a ser cacheado.

--- src/Furiza.Caching.Abstractions/ICacheHandler.cs
+++ src/Furiza.Caching.Abstractions/ICacheHandler.cs
@@ -1,11 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Furiza.Caching.Abstractions
 {
     public interface ICacheHandler
     {
         bool TryGetValue<T>(string key, out T value) where T : class;
-        Task SetAsync<T>(string key, T value) where T : class;
+        Task SetAsync<T>(string key, T value, IEnumerable<string> namesOfPropertiesToIgnore = null) where T : class;
         Task RemoveAsync<T>(string key) where T : class;
     }
 }
