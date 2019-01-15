22bbdd9761d2118c5396659f92ccc60a792a1007
Ivan Borges <ivanborges@live.com>
2019-01-15 14:17:54 -0200

    Inclusão de projeto para criação de HttpClients que já vem com header de authorization bearer embutido e com o access token preenchido a partir do UserPrincipal vinculado ao HttpContext.

---
+++ src/Furiza.Networking.Abstractions/IHttpClientFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http;
+
+namespace Furiza.Networking.Abstractions
+{
+    public interface IHttpClientFactory
+    {
+        HttpClient Create();
+
+        HttpClient Create(string baseAddress);
+    }
+}
